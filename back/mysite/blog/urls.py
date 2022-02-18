82c4a3ea124c649b015d00c124b50f1654649cd0
Byeon-Jaewon <byeonjae@kakao.com>
2022-02-18 14:21:10 +0900

    template추가 및 url 오타 수정

--- back/mysite/blog/urls.py
+++ back/mysite/blog/urls.py
@@ -1,8 +1,9 @@
-from django import views
 from django.urls import path
 
+from blog import views
+
 app_name = 'blog'
-urlpattern = [
+urlpatterns = [
     path('post/list/', views.PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
 ]
--- back/mysite/mysite/urls.py
+++ back/mysite/mysite/urls.py
@@ -1,6 +1,7 @@
 from django import views
 from django.contrib import admin
 from django.urls import include, path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
