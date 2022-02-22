019b6467f2a026871a32c855a9eceb90714bdc14
Byeon-Jaewon <byeonjae@kakao.com>
2022-02-22 15:35:33 +0900

    add layout

---
+++ front/src/components/MainFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-footer app>
+    <span>&copy; 2022 심심해서 해보는 vue-django </span>
+  </v-footer>
+</template>
--- front/src/pages/AppPostList.vue
+++ front/src/pages/AppPostList.vue
@@ -1,63 +1,25 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-    >
-
-    <v-list
-        dense
-        nav
-      >
-        <v-list-item
-          v-for="item in items"
-          :key="item.title"
-          link
-        >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <!--  -->
-    </v-navigation-drawer>
-
-    <v-app-bar app clipped-left color="indigo">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Application</v-toolbar-title>
-    </v-app-bar>
+    <MainMenu></MainMenu>
 
     <v-main>
       <PostList/>
     </v-main>
 
-    <v-footer app>
-      <span>&copy; 2022 </span>
-    </v-footer>
+    <MainFooter></MainFooter>
   </v-app>
 </template>
 
 <script>
+import MainMenu from '../components/MainMenu.vue';
+import MainFooter from "../components/MainFooter.vue";
 import PostList from '../components/PostList.vue';
 
 export default {
 
-  components: { PostList },
+  components: { PostList, MainMenu, MainFooter },
 
   data: () => ({ 
-    drawer: null,
-    items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' },
-      ],
     }),
 }
 </script>
